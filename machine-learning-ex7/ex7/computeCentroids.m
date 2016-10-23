f4ddbd8de19a81355b83950069c299c092fffb29
Philip Liao <phil@symphonycommerce.com>
2016-10-22 18:51:37 -0700

    [machine-learning] ex7: k-means section

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -1,7 +1,7 @@
 function centroids = computeCentroids(X, idx, K)
-%COMPUTECENTROIDS returs the new centroids by computing the means of the 
+%COMPUTECENTROIDS returns the new centroids by computing the means of the
 %data points assigned to each centroid.
-%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by
 %   computing the means of the data points assigned to each centroid. It is
 %   given a dataset X where each row is a single data point, a vector
 %   idx of centroid assignments (i.e. each entry in range [1..K]) for each
@@ -26,15 +26,21 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
+  function centroid = computeCentroid(X, idx, ki)
+    applicable_data = idx == ki; % vector of 0s and 1s
+    applicable_n = sum(applicable_data);
+    if applicable_n > 0
+      centroid = sum(X .* applicable_data) / applicable_n;
+    else
+      centroid = zeros(1, n);
+    end
+  end
 
-
-
-
-
-
+  for i = 1:K
+    centroids(i, :) = computeCentroid(X, idx, i)
+  end
 
 % =============================================================
 
 
 end
-
